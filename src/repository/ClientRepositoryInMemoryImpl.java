38a200bc5066bbf21518b6a868e9377ca3ff2280
OlegMels <melsitoleg1996@gmail.com>
2023-04-29 22:12:19 +0400

    O(n) to O(1) using HashMap

--- src/repository/ClientRepositoryInMemoryImpl.java
+++ src/repository/ClientRepositoryInMemoryImpl.java
@@ -5,13 +5,14 @@ import src.model.Client;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
 public class ClientRepositoryInMemoryImpl implements ClientRepository {
     //слой для связи с БД
     private final List<Client> clients = new ArrayList<>();
-    private final List<Account> accounts = new ArrayList<>();
+    private final HashMap<String, Account> accounts = new HashMap<>();
 
     public ClientRepositoryInMemoryImpl() {
         Client client1 = new Client(1, "Григорий Григорьевич Григорьев", LocalDate.of(1991, 5, 23));
@@ -20,17 +21,17 @@ public class ClientRepositoryInMemoryImpl implements ClientRepository {
         Account account13 = new Account(3, "40817810000000000003", client1);
 
         clients.add(client1);
-        accounts.add(account11);
-        accounts.add(account12);
-        accounts.add(account13);
+        accounts.put(account11.getNumber(), account11);
+        accounts.put(account12.getNumber(), account12);
+        accounts.put(account13.getNumber(), account13);
 
         Client client2 = new Client(2, "Василий Игнатьевич Пупкин", LocalDate.of(1980, 1, 7));
         Account account21 = new Account(4, "40817810000000000009", client2);
         Account account22 = new Account(5, "40817810000000000004", client2);
 
         clients.add(client2);
-        accounts.add(account21);
-        accounts.add(account22);
+        accounts.put(account21.getNumber(), account21);
+        accounts.put(account22.getNumber(), account22);
 
         Client client3 = new Client(3, "Петр Петрович Петров", LocalDate.of(2000, 10, 14));
         clients.add(client3);
@@ -38,9 +39,7 @@ public class ClientRepositoryInMemoryImpl implements ClientRepository {
 
     @Override
     public Client findByAccount(String accountNumber) {
-        Optional<Account> accountOptional = accounts.stream()
-                .filter(acc -> acc.getNumber().equals(accountNumber))
-                .findFirst();
+        Optional<Account> accountOptional = Optional.ofNullable(accounts.get(accountNumber));
 
         if (accountOptional.isPresent()) {
             return accountOptional
