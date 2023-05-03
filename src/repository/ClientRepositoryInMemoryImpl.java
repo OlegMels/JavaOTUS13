533b5f2c05ec43feb49c8913df2dc03f894c5a31
OlegMels <melsitoleg1996@gmail.com>
2023-05-03 21:26:37 +0400

    O(n) to O(1) using HashMap for Client

--- src/repository/ClientRepositoryInMemoryImpl.java
+++ src/repository/ClientRepositoryInMemoryImpl.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 public class ClientRepositoryInMemoryImpl implements ClientRepository {
     //слой для связи с БД
-    private final List<Client> clients = new ArrayList<>();
+    private final HashMap<Integer, Client> clients = new HashMap<>();
     private final HashMap<String, Account> accounts = new HashMap<>();
 
     public ClientRepositoryInMemoryImpl() {
@@ -20,7 +20,7 @@ public class ClientRepositoryInMemoryImpl implements ClientRepository {
         Account account12 = new Account(2, "40817810000000000002", client1);
         Account account13 = new Account(3, "40817810000000000003", client1);
 
-        clients.add(client1);
+        clients.put(client1.getId(), client1);
         accounts.put(account11.getNumber(), account11);
         accounts.put(account12.getNumber(), account12);
         accounts.put(account13.getNumber(), account13);
@@ -29,12 +29,12 @@ public class ClientRepositoryInMemoryImpl implements ClientRepository {
         Account account21 = new Account(4, "40817810000000000009", client2);
         Account account22 = new Account(5, "40817810000000000004", client2);
 
-        clients.add(client2);
+        clients.put(client2.getId(), client2);
         accounts.put(account21.getNumber(), account21);
         accounts.put(account22.getNumber(), account22);
 
         Client client3 = new Client(3, "Петр Петрович Петров", LocalDate.of(2000, 10, 14));
-        clients.add(client3);
+        clients.put(client3.getId(), client3);
     }
 
     @Override
@@ -52,6 +52,12 @@ public class ClientRepositoryInMemoryImpl implements ClientRepository {
 
     @Override
     public List<Client> findAll() {
-        return clients;
+        List<Client> clientsAsList = new ArrayList<>();
+
+        for(int i = 0; i < clients.size(); i++){
+            clientsAsList.add(clients.get(i+1));
+        }
+
+        return clientsAsList;
     }
 }
